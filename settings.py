a1333354e1119d1b211624cec10231f41c6b843c
ladavids <54285452+ladavids@users.noreply.github.com>
2022-03-22 14:19:59 -0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,13 @@
+#/usr/bin/env python3
+APP_HOST = 'cs3103.cs.unb.ca'
+APP_PORT = 43075
+APP_DEBUG = True
+
+MYSQL_HOST = 'localhost'
+MYSQL_USER = 'ldavids2'
+MYSQL_PASSWD = '8WNJR2uO'
+MYSQL_DB = MYSQL_USER
+
+SECRET_KEY = 'd41d8cd98f00b204e9800998ecf8427e'
+
+LDAP_HOST =  'ldap-student.cs.unb.ca'
