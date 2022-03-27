6c608c65c83016961b9ade21b3ecfd04a564bb39
Short-T <56893242+Short-T@users.noreply.github.com>
2022-03-26 22:20:25 -0300

    Fixed MYSQL_ to DB_ for compatibility with service

--- src/application/settings.py
+++ src/application/settings.py
@@ -3,10 +3,10 @@ APP_HOST = 'cs3103.cs.unb.ca'
 APP_PORT = 43075
 APP_DEBUG = True
 
-MYSQL_HOST = 'localhost'
-MYSQL_USER = 'ldavids2'
-MYSQL_PASSWD = '8WNJR2uO'
-MYSQL_DB = MYSQL_USER
+DB_HOST = 'localhost'
+DB_USER = 'ldavids2'
+DB_PASSWD = '8WNJR2uO'
+DB_DATABASE = DB_USER
 
 SECRET_KEY = 'd41d8cd98f00b204e9800998ecf8427e'
 
