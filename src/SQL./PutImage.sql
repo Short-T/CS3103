e7b614ec987a303f036e26729f3cdef7638d69fb
Short-T <56893242+Short-T@users.noreply.github.com>
2022-03-27 21:33:31 -0300

    Update PutImage.sql
    added select last insert

--- src/SQL./PutImage.sql
+++ src/SQL./PutImage.sql
@@ -4,5 +4,11 @@ CREATE PROCEDURE putImage(IN fileExtension VARCHAR(55), IN fName VARCHAR(50), IN
 BEGIN   
     INSERT INTO images(ImageFileExtension, ImageFileName, ImagePath) VALUE
         (fileExtension, fName, fPath);
+    IF(ROW_COUNT() = 0) THEN
+      SIGNAL SQLSTATE '52713'
+        SET MESSAGE_TEXT = 'Unable to create the image';
+    END IF;
+    
+    SELECT LAST_INSERT_ID();
 END //
 DELIMITER ;
