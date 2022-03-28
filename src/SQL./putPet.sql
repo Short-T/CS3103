7e8e9ab522d021cec5dad08c753162e00dd96e61
Short-T <56893242+Short-T@users.noreply.github.com>
2022-03-27 21:28:46 -0300

    Update putPet.sql
    added select insert id

--- src/SQL./putPet.sql
+++ src/SQL./putPet.sql
@@ -4,5 +4,13 @@ CREATE PROCEDURE putPet(IN species VARCHAR(55), IN breed VARCHAR(50), IN pName V
 BEGIN   
     INSERT INTO pets(PetSpecies, PetBreed, PetName, PetAge) VALUE
         (species, breed, pName, age);
+    
+    IF(ROW_COUNT() = 0) THEN
+      SIGNAL SQLSTATE '52712'
+        SET MESSAGE_TEXT = 'Unable to create the pet';
+    END IF;
+
+    / If the INSERT is successful, then this will return the Id for the record /
+    SELECT LAST_INSERT_ID(); / Specific to this session */
 END //
 DELIMITER ;
