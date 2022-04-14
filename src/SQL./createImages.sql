2baa8ffb665b7d2118bb5a209a5920456f4e8bcf
Tcampbe6 <60269582+tcampbe6UNB3035@users.noreply.github.com>
2022-04-13 21:06:01 -0300

    Update createImages.sql

--- src/SQL./createImages.sql
+++ src/SQL./createImages.sql
@@ -1,9 +1,7 @@
 DROP TABLE IF EXISTS images;
 CREATE TABLE images (
     IdImage INT NOT NULL AUTO_INCREMENT,
-    ImageFileExtension VarChar(50),
     ImageFileName VarChar(50),
-    ImagePath VarChar(50),
     PetId Int,
     ImageTitle VarChar(50),
     ImageDescription VarChar(255),
