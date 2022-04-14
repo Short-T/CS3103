af78370a554ebfc3f12617dfcd50b492b9981af8
Tcampbe6 <60269582+tcampbe6UNB3035@users.noreply.github.com>
2022-04-13 21:06:53 -0300

    Update putImage.sql

--- src/SQL./putImage.sql
+++ src/SQL./putImage.sql
@@ -1,9 +1,9 @@
 DELIMITER //
 DROP PROCEDURE IF EXISTS putImage //
-CREATE PROCEDURE putImage(IN fileExtension VARCHAR(50), IN fPath VARCHAR(50), IN fName VARCHAR(50), IN pet INT, IN iTitle VARCHAR(50), IN description VARCHAR(255))
+CREATE PROCEDURE putImage(IN fName VARCHAR(50), IN pet INT, IN iTitle VARCHAR(50), IN description VARCHAR(255))
 BEGIN   
-    INSERT INTO images(ImageFileExtension, ImageFileName, ImagePath, PetId, ImageTitle, ImageDescription) VALUE
-        (fileExtension, fPath, fName, pet, iTitle, description);
+    INSERT INTO images(ImageFileName, PetId, ImageTitle, ImageDescription) VALUE
+        (fName, pet, iTitle, description);
     IF(ROW_COUNT() = 0) THEN
       SIGNAL SQLSTATE '52713'
         SET MESSAGE_TEXT = 'Unable to create the image';
